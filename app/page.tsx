5999bd6447a8ceb442238811f207e424809e2d4f
Ola Wale <bolawale81@gmail.com>
2024-10-27 17:31:53 +0100

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+
+export const metadata = {
+    title: "Home | StartupFunders",
+    description: "Where Ideas become reality",
+};
+
+export default function Home() {
+
+  return (
+        <h1 className="text-3xl">Welcome to Next.js</h1>
+  );
+}

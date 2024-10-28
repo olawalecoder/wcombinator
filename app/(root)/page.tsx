91d33922cd2b798f242688cd53990e1ccb71fe39
olawalecoder <bolawale81@gmail.com>
2024-10-28 17:41:28 +0100

    Added Header and subtext

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,16 +1,30 @@
+import SearchForm from "@/app/components/SearchForm";
 
 export const metadata = {
     title: "Home | StartupFunders",
     description: "Where Ideas become reality",
 };
 
-export default function Home() {
+export default async function Home({searchParams} :{
+    searchParams: Promise<{ query?: string }>
+}) {
+    const query = (await searchParams).query;
 
     return (
         <>
-        <h1 className="text-2xl">Home</h1>
+            <section className="pink_container">
+                <h1 className="heading">Pitch your startup, <br />
+                    Connect with Entrepreneurs </h1>
+                <p className="sub-heading !max-w-3xl">
+                    Submit Ideas, Vote on Pitches, and Get Noticed In Virtual Competitions
+                </p>
+
+                    <SearchForm query={query} />
+
+            </section>
+
 
         </>
     )
-    ;
+        ;
 }

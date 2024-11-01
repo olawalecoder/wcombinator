19d857713bdc0480e6f3b3053d9f1e816e48c9a3
olawalecoder <bolawale81@gmail.com>
2024-11-01 19:18:34 +0100

    form in progress

--- components/StartupForm.tsx
+++ components/StartupForm.tsx
@@ -1,17 +1,58 @@
 "use client"
 
 import {Input} from "@/components/ui/input";
-import {useState} from "react";
+import React, {useState, useActionState} from "react";
 import {Textarea} from "@/components/ui/textarea";
+import MDEditor from "@uiw/react-md-editor";
 import {Button} from "@/components/ui/button";
+import {Send} from "lucide-react";
+import {formSchema} from "@/lib/validation";
+import {z} from "zod";
+
 
 
 const StartupForm = () => {
-    const [ errors, setErrors] = useState<Record<string, string>>({});
+    const [errors, setErrors] = useState<Record<string, string>>({});
+
+    const [pitch, setPitch] = useState("");
+
+    const handleFormSubmit = async (prevState: any, formData: FormData) => {
+        try {
+            const formValues = {
+                title: formData.get("title") as string,
+                description: formData.get("description") as string,
+                category: formData.get("category") as string,
+                link: formData.get("link") as string,
+                pitch,
+            };
+
+            await formSchema.parseAsync(formValues);
+            console.log(formValues);
+            // const result = await createIdea(prevState, formData, pitch);
+
+            // console.log(result);
+        } catch (error) {
+            if (error instanceof z.ZodError) {
+                const fieldErrors = error.flatten().fieldErrors;
+
+                setErrors(fieldErrors as unknown as Record<string, string>);
+
+                return { ...prevState, error: "Validation failed", status: "ERROR" };
+            }
+            return { ...prevState, eerror: "An unexpected error has occurred",
+                status: "ERROR", };
+
+
+        }
+    };
+
+    const [state, formAction, isPending] = useActionState(handleFormSubmit, {
+        error: "",
+        status: "INITIAL",
+    });
 
     return (
-        <form action={() => {
-        }} className="startup-form">
+        <form action={formAction} className="startup-form">
             <div>
                 <label htmlFor="title" className="startup-form_label">
                     Title
@@ -71,6 +112,33 @@ const StartupForm = () => {
 
                 {errors.link && <p className="startup-form_error"> {errors.link} </p>}
             </div>
+
+            <div data-color-mode="light">
+                <label htmlFor="pitch" className="startup-form_label">
+                    Your Pitch
+                </label>
+                <MDEditor
+                    value={pitch}
+                    onChange={(value) => setPitch(value as string)}
+                    id="pitch"
+                    preview="edit"
+                    height={300}
+                    style={{ borderRadius: 20, overflow: "hidden" }}
+                    textareaProps={{
+                        placeholder: "Briefly descibe your idea. Blow our Minds",
+                    }}
+                    previewOptions={{
+                        disallowedElements: ["style"],
+                    }}
+                />
+
+                {errors.pitch && <p className="startup-form_error"> {errors.pitch} </p>}
+            </div>
+
+            <Button type="submit" className="startup-form_btn text-white" disabled={isPending}>
+                {isPending ? 'Submitting...' : 'Blow Our Minds'}
+                <Send  className="" />
+            </Button>
 
         </form>
     )

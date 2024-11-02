3a0f3b19f1e61bdaa700848965216a840f245fd6
olawalecoder <bolawale81@gmail.com>
2024-11-02 05:34:40 +0100

    Added editor picks and tested the site

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -1,5 +1,5 @@
 import React, {Suspense} from 'react'
-import {STARTUPS_BY_ID_QUERY} from "@/sanity/lib/queries";
+import { PLAYLIST_BY_SLUG_QUERY, STARTUPS_BY_ID_QUERY} from "@/sanity/lib/queries";
 import {client} from "@/sanity/lib/client";
 import {notFound} from "next/navigation";
 import {formatDate} from "@/lib/utils";
@@ -8,6 +8,7 @@ import Image from "next/image";
 import markdownit from "markdown-it";
 import {Skeleton} from "@/components/ui/skeleton";
 import View from "@/components/View";
+import StartupCard, {StartupTypeCard} from "@/components/StartupCard";
 
 const md = markdownit();
 
@@ -16,7 +17,12 @@ export const experimental_ppr = true;
 const Page = async ({ params } : {params: Promise<{ id: string }>}) => {
     const id = (await params).id;
 
-    const post = await client.fetch(STARTUPS_BY_ID_QUERY, { id });
+    const [post, { select: editorPosts }] = await Promise.all([
+        client.fetch(STARTUPS_BY_ID_QUERY, { id }),
+        client.fetch(PLAYLIST_BY_SLUG_QUERY, {
+            slug: "editor-picks-new",
+        }),
+    ]);
 
     if (!post) return notFound();
 
@@ -64,6 +70,20 @@ const Page = async ({ params } : {params: Promise<{ id: string }>}) => {
                 </div>
 
                 <hr className="divider" />
+
+                {editorPosts && editorPosts.length > 0 ? (
+                    <div className="max-w-4xl mx-auto">
+                        <p className="text-30-semibold">Editor Picks</p>
+
+                        <ul className="mt-7 card_grid-sm">
+                            {editorPosts.map((post: StartupTypeCard, i: number) => (
+                                <StartupCard key={i} post={post} />
+                            ))}
+                        </ul>
+                    </div>
+                ) : (
+                    <p className="no-result">No editor picks available</p>
+                )}
 
                <Suspense fallback={<Skeleton className="view_skeleton" />}>
                    <View id={id} />
--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -54,8 +54,7 @@ export const AUTHOR_BY_ID_QUERY = defineQuery(`*[_type == "author" && _id == $id
     bio
 }`)
 
-export const STARTUPS_BY_AUTHOR_QUERY =
-    defineQuery(`*[_type == "startup" && author._ref == $id] | order(_createdAt desc) {
+export const STARTUPS_BY_AUTHOR_QUERY = defineQuery(`*[_type == "startup" && author._ref == $id] | order(_createdAt desc) {
       _id,
       title,
       slug,
@@ -68,3 +67,28 @@ export const STARTUPS_BY_AUTHOR_QUERY =
       category,
       image
     }`)
+
+
+export const PLAYLIST_BY_SLUG_QUERY = defineQuery(`*[_type == "playlist" && slug.current == $slug][0]{
+  _id,
+  title,
+  slug,
+  select[]->{
+    _id,
+    _createdAt,
+    title,
+    slug,
+    author->{
+      _id,
+      name,
+      slug,
+      image,
+      bio
+    },
+    views,
+    description,
+    category,
+    image,
+    pitch
+  }
+}`);

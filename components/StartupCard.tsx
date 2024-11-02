d9767ce21f26da92b78bf1aeadb77cbfa9216645
olawalecoder <bolawale81@gmail.com>
2024-11-02 03:51:17 +0100

    create userprofile and display user startupd

--- components/StartupCard.tsx
+++ components/StartupCard.tsx
@@ -1,9 +1,11 @@
-import {formatDate} from "@/lib/utils";
+import {cn, formatDate} from "@/lib/utils";
 import {EyeIcon} from "lucide-react";
 import Link from "next/link";
 import Image from "next/image";
 import {Button} from "@/components/ui/button";
 import {Author, Startup} from "@/sanity/types";
+import { Skeleton } from "@/components/ui/skeleton";
+
 
 export type StartupTypeCard = Omit<Startup, "author"> & {author?: Author}
 
@@ -37,7 +39,8 @@ const StartupCard = ({ post }: { post: StartupTypeCard}) => {
                 </div>
 
                 <Link href={`/user/${author?._id}`}>
-                    <Image src="https://placehold.co/48x48" alt="placeholder" width={48} height={48}
+                    <Image
+                        src={author?.image} alt={author?.name} width={48} height={48}
                            className="rounded-full" />
                 </Link>
             </div>
@@ -62,6 +65,18 @@ const StartupCard = ({ post }: { post: StartupTypeCard}) => {
 
             </div>
         </li>
+    );
+};
+export const StartupCardSkeleton = () => {
+    return (
+        <>
+            {[0,1,2,3,4].map((index: number) => (
+                <li key={cn("skeleton", index)}>
+                    <Skeleton className="startup-card_skeleton" />
+                </li>
+            ))}
+        </>
     )
 }
+
 export default StartupCard

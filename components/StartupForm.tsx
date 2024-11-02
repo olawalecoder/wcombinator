33c4327e65e22a260b5c5f4b2b3f0aaa1429926a
olawalecoder <bolawale81@gmail.com>
2024-11-02 06:46:39 +0100

    Form error feedback is changed and form doesn't reset after submit

--- components/StartupForm.tsx
+++ components/StartupForm.tsx
@@ -13,10 +13,12 @@ import {useRouter} from "next/navigation";
 import {createPitch} from "@/lib/actions";
 
 
-
 const StartupForm = () => {
     const [errors, setErrors] = useState<Record<string, string>>({});
-
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+    const [category, setCategory] = useState("");
+    const [link, setLink] = useState("");
     const [pitch, setPitch] = useState("");
 
     const {toast} = useToast();
@@ -24,19 +26,13 @@ const StartupForm = () => {
 
     const handleFormSubmit = async (prevState: any, formData: FormData) => {
         try {
-            const formValues = {
-                title: formData.get("title") as string,
-                description: formData.get("description") as string,
-                category: formData.get("category") as string,
-                link: formData.get("link") as string,
-                pitch,
-            };
+            const formValues = { title, description, category, link, pitch };
 
             await formSchema.parseAsync(formValues);
-            console.log(formValues);
-            const result = await createPitch(prevState, formData, pitch);
 
-            if (result.status == "SUCCESS") {
+            const result = await createPitch(prevState, formValues);
+
+            if (result.status === "SUCCESS") {
                 toast({
                     title: "Success",
                     description: "Your Startup pitch has been created successfully!",
@@ -50,7 +46,6 @@ const StartupForm = () => {
         } catch (error) {
             if (error instanceof z.ZodError) {
                 const fieldErrors = error.flatten().fieldErrors;
-
                 setErrors(fieldErrors as unknown as Record<string, string>);
 
                 toast({
@@ -68,10 +63,7 @@ const StartupForm = () => {
                 variant: "destructive",
             });
 
-            return { ...prevState, error: "An unexpected error has occurred",
-                status: "ERROR", };
-
-
+            return { ...prevState, error: "An unexpected error has occurred", status: "ERROR" };
         }
     };
 
@@ -83,93 +75,86 @@ const StartupForm = () => {
     return (
         <form action={formAction} className="startup-form">
             <div>
-                <label htmlFor="title" className="startup-form_label">
-                    Title
-                </label>
+                <label htmlFor="title" className="startup-form_label">Title</label>
                 <Input
                     id="title"
                     name="title"
                     className="startup-form_input"
                     required
                     placeholder="Startup Title"
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
                 />
-
-                {errors.title && <p className="startup-form_error"> {errors.title} </p>}
+                {errors.title && <p className="startup-form_error">{errors.title}</p>}
             </div>
 
             <div>
-                <label htmlFor="description" className="startup-form_label">
-                    Description
-                </label>
+                <label htmlFor="description" className="startup-form_label">Description</label>
                 <Textarea
                     id="description"
                     name="description"
                     className="startup-form_textarea"
                     required
                     placeholder="Startup description"
+                    value={description}
+                    onChange={(e) => setDescription(e.target.value)}
                 />
-
-                {errors.description && <p className="startup-form_error"> {errors.description} </p>}
+                {errors.description && <p className="startup-form_error">{errors.description}</p>}
             </div>
 
             <div>
-                <label htmlFor="category" className="startup-form_label">
-                    Category
-                </label>
+                <label htmlFor="category" className="startup-form_label">Category</label>
                 <Input
                     id="category"
                     name="category"
                     className="startup-form_input"
                     required
-                    placeholder="Startup category (Tech, Health, Eduction...)"
+                    placeholder="Startup category (Tech, Health, Education...)"
+                    value={category}
+                    onChange={(e) => setCategory(e.target.value)}
                 />
-
-                {errors.title && <p className="startup-form_error"> {errors.title} </p>}
+                {errors.category && <p className="startup-form_error">{errors.category}</p>}
             </div>
 
             <div>
-                <label htmlFor="link" className="startup-form_label">
-                    Image URL
-                </label>
+                <label htmlFor="link" className="startup-form_label">Image URL</label>
                 <Input
                     id="link"
                     name="link"
                     className="startup-form_input"
                     required
                     placeholder="Paste a link to your demo or promotional media"
+                    value={link}
+                    onChange={(e) => setLink(e.target.value)}
                 />
-
-                {errors.link && <p className="startup-form_error"> {errors.link} </p>}
+                {errors.link && <p className="startup-form_error">{errors.link}</p>}
             </div>
 
             <div data-color-mode="light">
-                <label htmlFor="pitch" className="startup-form_label">
-                    Your Pitch
-                </label>
+                <label htmlFor="pitch" className="startup-form_label">Your Pitch</label>
                 <MDEditor
                     value={pitch}
-                    onChange={(value) => setPitch(value as string)}
+                    onChange={(value) => setPitch(value || "")}
                     id="pitch"
                     preview="edit"
                     height={300}
                     style={{ borderRadius: 20, overflow: "hidden" }}
                     textareaProps={{
-                        placeholder: "Briefly descibe your idea. Blow our Minds",
+                        placeholder: "Briefly describe your idea. Blow our Minds",
                     }}
                     previewOptions={{
                         disallowedElements: ["style"],
                     }}
                 />
-
-                {errors.pitch && <p className="startup-form_error"> {errors.pitch} </p>}
+                {errors.pitch && <p className="startup-form_error">{errors.pitch}</p>}
             </div>
 
             <Button type="submit" className="startup-form_btn text-white" disabled={isPending}>
                 {isPending ? 'Submitting...' : 'Blow Our Minds'}
-                <Send  className="" />
+                <Send className="" />
             </Button>
-
         </form>
     )
 }
-export default StartupForm
+
+export default StartupForm;
